65d6d927fbd795a395a05e21b61ab1c7937ba34d
unknown <895739909@qq.com>
2016-04-08 17:36:28 +0800

    try do get

--- src/main/prac/sendReqest.py
+++ src/main/prac/sendReqest.py
@@ -1,16 +1,27 @@
 import urllib2
+import urllib
 
-
-
-
-base_url = "https://book.douban.com/subject/2035162/"
 
 def doGet(url):
     request = urllib2.Request(url)
     response = urllib2.urlopen(request)
     print response.read()
 
+def doPost():
+    values = {}
+    values['username'] = "1016903103@qq.com"
+    values['password'] = "XXXX"
+    data = urllib.urlencode(values)
+    url = "http://passport.csdn.net/account/login?from=http://my.csdn.net/my/mycsdn"
+    request = urllib2.Request(url,data)
+    response = urllib2.urlopen(request)
+    print response.read()
+
+
+
+base_url = "https://book.douban.com/subject/2035162/"
 doGet(base_url)
+
 
 
 
